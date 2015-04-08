f6461f6d0d963265d17aa7a3f4ec810b2c2c5b7f
ChillieDude <chillieman224@aim.com>
2015-04-08 14:54:37 -0400

    Update Main.cpp
    Changed SDL_Error() -> SDL_GetError()

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main( int argc, char* args[] )
   //When SDL cannot be initialized, it will return a value of -1
   if( SDL_Init(SDL_INIT_EVERYTHING) == -1)
   {
-    std::cout << "Could not initialize SDL: " << SDL_Error() << std::endl;
+    std::cout << "Could not initialize SDL: " << SDL_GetError() << std::endl;
   }
   
   else
