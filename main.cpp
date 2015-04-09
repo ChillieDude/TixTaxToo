9790a79d3193c73c89c56d1b47365697a10f3eb8
ChillieDude <chillieman224@aim.com>
2015-04-09 00:45:35 -0400

    Fixed main.cpp
    Forgot to initialize the variables we need for the program to run correctly

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 #include <iostream>
 #include <SDL.h>
 
+//Always initialize pointers to NULL
+SDL_Window* theWindow = NULL;
+SDL_Renderer* theRenderer = NULL;
+
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+const int LINE_W = 3;
+
 int main( int argc, char* args[] )
 {
   //When SDL cannot be initialized, it will return a value of -1
