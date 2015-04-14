a9b8cdf6a0da86768782e578876c3cbe3928c1e5
ChillieDude <chillieman224@aim.com>
2015-04-14 05:14:17 -0400

    Update Constants
    Maybe i should make a seperate .cpp for these?

--- main.cpp
+++ main.cpp
@@ -1,31 +1,36 @@
-//SDL Obviously works at this point
-
 #include "Globals.h"
 
 
 //Always initialize pointers to NULL
 SDL_Window* theWindow = NULL;
 SDL_Renderer* theRenderer = NULL;
+
 //We need a surface in order to load images - and then use a renderer to convert the surface into a texture
 SDL_Surface* theSurface = NULL;
-SDL_Texture* theTexture = NULL;
 
+//init Screen Width and Height
 const int SCREEN_W = 640;
 const int SCREEN_H = 480;
-//Line MUST be an even number (For 4 Tiles)
-const int LINE_W = 2;
-const int TILE_H = SCREEN_H / 2 - LINE_W / 2;
-const int TILE_W = SCREEN_W / 2 - LINE_W / 2;
-const int TILE_TOTAL = 4;
+const int ROWS_TOTAL = 3;
+const int LINE_W = 10;
 
+
+//Auto Updating Constants -----------
+
+//There will be one less line then there are tiles for each Axis (X & Y)
+const int LINE_TOTAL = (ROWS_TOTAL - 1) * 2;
+
+const int TILE_W = (SCREEN_W - LINE_W * (LINE_TOTAL / 2) ) / ROWS_TOTAL;
+const int TILE_H = (SCREEN_H - LINE_W * (LINE_TOTAL / 2) ) / ROWS_TOTAL;
+const int TILE_TOTAL = ROWS_TOTAL * ROWS_TOTAL;
+
+SDL_Rect tLines[LINE_TOTAL];
 SDL_Rect tSprites[SPRITE_TOTAL];
 tTile tTiles[TILE_TOTAL];
-
-//The tho bellow variables will be deleted eventually
-int boxH = SCREEN_H / 2 - LINE_W;
-int boxW = SCREEN_W / 2 - LINE_W;
-
 tTexture theSpriteSheet;
+
+//Let us go first!
+bool isMyTurn = true;
 
 int main( int argc, char* args[] )
 {
