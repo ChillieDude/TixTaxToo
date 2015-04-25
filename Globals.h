9e1751d5592227fc65c8d50b3ccb227739d4f822
ChillieDude <chillieman224@aim.com>
2015-04-25 15:27:51 -0400

    Update Globals.h

--- Globals.h
+++ Globals.h
@@ -2,9 +2,43 @@
 #define GLOBALS_H_INCLUDED
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
+void SetWelcome();
+void Test();
+
+extern const int MENU_H;
+extern const int MENU_W;
+extern const int MENU_X;
+extern const int MENU_Y;
+
+extern TTF_Font* theFont;
+extern SDL_Color textColor;
+
+
+enum gameLevel
+{
+    LEVEL_EASY = 0,
+    LEVEL_MEDIUM = 1,
+    LEVEL_HARD = 2,
+    LEVEL_TOTAL = 3
+};
+extern gameLevel theGameLevel;
+
+enum gameState
+{
+    GAMESTATE_WELCOME = 0,
+    GAMESTATE_PLAY = 1,
+    GAMESTATE_MENU = 2,
+    GAMESTATE_STATS = 3,
+    GAMESTATE_RESULTS = 4,
+    GAMESTATE_TOTAL = 5
+};
+extern gameState theGameState;
+
 
 class tTexture
 {
@@ -13,20 +47,24 @@ public:
     ~tTexture();
 
     void Deallocate();
-    bool LoadImage(std::string filename);
+    bool LoadImage(const std::string filename);
     void RenderX( SDL_Rect* target, SDL_Rect* clip);
 
+    bool LoadText(const std::string text, const SDL_Color textColor, const int fontSize = 32);
     //Easier to use Render VV
-    void Render(int x, int y, SDL_Rect* clip);
+    void Render();
 
     //Functions for calling height / width
     int GetHeight();
     int GetWidth();
+    int GetY();
+    int GetX();
+
+    void SetLocation(int x, int y);
 
 private:
     SDL_Texture* sTexture;
-    int tWidth;
-    int tHeight;
+    SDL_Rect renderLocation;
 };
 
 
@@ -35,6 +73,7 @@ extern SDL_Window* theWindow;
 extern SDL_Renderer* theRenderer;
 extern SDL_Surface* theSurface;
 extern tTexture theSpriteSheet;
+extern TTF_Font* theFont;
 
 
 //One of the few times you wont supply a const with a value up front
@@ -49,13 +88,16 @@ extern const int TILE_H;
 
 
 
+
 extern bool isMyTurn;
 
 
-
+void DestroyWelcomeTextures();
+void LoadWelcomeStrings();
 //Lets also declare our functions
 bool InitSDL();
 bool LoadContent();
+void LoadButtons();
 void Close();
 void Play();
 void ComputersTurn();
@@ -63,7 +105,11 @@ void Menu();
 
 void DrawBoard();
 void DrawMenu();
+void DrawWelcome();
 void ResetGame();
+void SetMenu();
+
+
 
 
 //Sprite Flags  : BEGIN
@@ -112,25 +158,53 @@ private:
 extern tTile tTiles[];
 extern SDL_Rect tLines[];
 
+//tButtons
+enum buttonFlag
+{
+    BUTTON_RESUME = 0,
+    BUTTON_STATS = 1,
+    BUTTON_STATS_RETURN = 2,
+    BUTTON_EASY = 3,
+    BUTTON_MEDIUM = 4,
+    BUTTON_HARD = 5,
+    BUTTON_EXIT = 6,
+    BUTTON_TOTAL = 7
+};
+
 
 class tButton
 {
 public:
+    tButton(std::string theText);
     tButton();
+    ~tButton();
 
-    void HandleEvent(SDL_Event* e);
+    int GetW();
+    int GetH();
+    bool Outlined();
+
+    void ChangeOutline(const bool flag);
+    //returns true if the user wants to exit
+    bool HandleEvent(SDL_Event* e);
     void SetLocation(int x, int y);
     void Render();
+    void RenderOutlined();
+    void Deallocate();
+    void SetButtonFlag(buttonFlag btnFlag);
+    void SetWidthHeight(int w, int h);
+    void SetText(const std::string theText);
 
 
 private:
     SDL_Rect currentLocation;
-    std::string text;
+    buttonFlag tButtonFlag;
+    std::string textString;
+    tTexture textTexture;
+    bool outlined;
 };
 
-
-
-//AI Functions
+extern tButton tButtons[BUTTON_TOTAL];
+//End
 
 enum winFlag
 {
@@ -146,8 +220,14 @@ enum winFlag
     TILE_TOTAL = 9
 };
 
+//AI Functions
 bool IWin(char givenCharacter);
+bool winCondition(int* slots, int size, winFlag* flags);
 bool win_logic(int* slots, int size, int flag);
+void findAttemptedPath(int*& slots, int size, int& tset);
+const int slotsTaken(char c);
+
+
 
 
 #endif // GLOBALS_H_INCLUDED
